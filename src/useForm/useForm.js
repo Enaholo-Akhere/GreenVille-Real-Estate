c632294b3b15f092e7d4f95446de4f1fa1f0e72a
i-codeena <enaholoa@gmail.com>
2021-07-24 23:16:16 +0100

    useform updated

--- src/useForm/useForm.js
+++ src/useForm/useForm.js
@@ -4,18 +4,27 @@ import Validation from '../Validate/validation'
 const useForm = (submitForm) => {
     const [dataIsCorrect, setDataIsCorrect] = useState(false)
     const [errors, setError] = useState({});
+    const [length, setLength] = useState(null);
     const [values, setValues] = useState({
-        fname:"",
-        lname: "",
-        phone: "",
-        bio: "",
-        password: "",
+        FirstName:"",
+        LastName: "",
+        Phone: "",
+        Bio: "",
+        Worth: ""
     });
 
 const handleSubmit = (event) => {
     event.preventDefault();
     setError(Validation(values));
     setDataIsCorrect(true);
+    fetch('http://localhost:8000/reg', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(values)
+    }).then(()=>{
+        console.log("submit")
+    })
+    setDataIsCorrect(true)
 }
 
 const handleChange = (event) =>{
@@ -23,6 +32,11 @@ const handleChange = (event) =>{
         ...values,
     [event.target.name]: event.target.value
 })
+  
+    if(event.target.name === 'Bio'){
+        setLength(300 - event.target.value.length)
+    }
+
 }
  useEffect(()=>{
     if(Object.keys(errors).length === 0 && dataIsCorrect){
@@ -30,7 +44,7 @@ const handleChange = (event) =>{
     }
  },[errors, submitForm, dataIsCorrect])
 
-return {handleSubmit, handleChange, values, errors};
+return {handleSubmit, handleChange, values, errors, length};
 
 }
 export default useForm;
